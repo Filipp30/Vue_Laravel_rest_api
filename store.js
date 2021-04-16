94277fc2b187377be3884971bf9e3ced83c52542
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-16 20:02:21 +0200

    Refactoring/Testing

--- store.js
+++ store.js
@@ -4,13 +4,13 @@ import Pusher from "pusher-js";
 
 const store = createStore({
     state:{
-        axios_request_url: "http://127.0.0.1:8000",
+        axios_request_url: "http://stuworld.space",
         contact_chat_channel:new Pusher('8a34625906a44e573ba7',{
             useTLS: true,
             forceTLS: true,
             encrypted: true,
             cluster: "eu",
-            authEndpoint: 'http://127.0.0.1:8000/api/pusher/auth',
+            authEndpoint: 'http://stuworld.space/api/pusher/auth',
             auth:{
                 headers:{
                     Authorization:`Bearer ${localStorage.getItem('jwt_token')}`
