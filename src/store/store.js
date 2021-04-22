78692fffe69ed8ff885f60d055b0bc0217aa9b27
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-04-22 23:23:55 +0200

    refactoring pusher to vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,17 @@
 import { createStore } from "vuex"
-import Pusher from "pusher-js";
 import chat from "./modules/chat";
-
+import Pusher from "pusher-js";
 
 const store = createStore({
     state:{
-        axios_request_url: "http://stuworld.space",
+        axios_request_url: "http://127.0.0.1:8000",
 
         contact_chat_channel:new Pusher('8a34625906a44e573ba7',{
             useTLS: true,
             forceTLS: true,
             encrypted: true,
             cluster: "eu",
-            authEndpoint: 'http://stuworld.space/api/pusher/auth',
-            // auth:{
-            //     headers:{
-            //         Authorization:`Bearer ${localStorage.getItem('jwt_token')}`
-            //     }
-            // }
+            // authorizer: ()=>{return response.data}
         }).subscribe('private-my-channel'),
 
     },
